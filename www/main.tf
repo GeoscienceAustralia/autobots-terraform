97d64642e89b5a26c4e58c27f577f2abeed0e7f0
Shane Nayler <snayler0@hotmail.com>
2016-12-15 14:12:09 +1100

    Add VPC

--- www/main.tf
+++ www/main.tf
@@ -2,10 +2,31 @@ provider "aws" {
     region = "ap-southeast-2"
 }
 
+resource "aws_vpc" "www-vpc" {
+    cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "default" {
+    vpc_id = "${aws_vpc.www-vpc.id}"
+}
+
+resource "aws_route" "internet-access" {
+    route_table_id = "${aws_vpc.www-vpc.main_route_table_id}"
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.default.id}"
+}
+
+resource "aws_subnet" "www-subnet" {
+    vpc_id = "${aws_vpc.www-vpc.id}"
+    cidr_block = "10.0.1.0/24"
+    map_public_ip_on_launch = true
+}
+
 resource "aws_instance" "www" {
     ami = "ami-4d3b062e"
     instance_type = "t2.micro"
-    security_groups = ["${aws_security_group.www.name}"]
+    vpc_security_group_ids = ["${aws_security_group.www.id}"]
+    subnet_id = "${aws_subnet.www-subnet.id}"
     user_data = <<-EOF
     #!/bin/bash
     echo "Hello World" >> index.html
@@ -15,7 +36,8 @@ resource "aws_instance" "www" {
 
 resource "aws_security_group" "www" {
     name = "wwwterraformsg"
-    description = "Shane is the best"
+    vpc_id = "${aws_vpc.www-vpc.id}"
+    description = "Autobots are the best"
     ingress {
         from_port = "80"
         to_port = "80"
